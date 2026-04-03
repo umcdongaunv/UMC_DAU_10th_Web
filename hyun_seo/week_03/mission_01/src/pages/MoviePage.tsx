e0fba8802d30c6770c3c4914843fe4f5eb3cdaf1
seohyunnii <ejsj39@nate.com>
2026-04-03 14:13:36 +0900

    feat: 현서 3주차 미션1 제출

---
+++ hyun_seo/week_03/mission_01/src/pages/MoviePage.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import type { Movie, MovieResponse } from "../types/movie";
+import MovieCard from "../components/MovieCard";
+
+export default function MoviePage() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  // 마운트될 때 한 번만 실행시켜주고 싶어서 useEffect
+  useEffect(() => {
+    const fetchMovies = async() => {
+      const {data} = await axios.get<MovieResponse>(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`
+          },
+        }
+      );
+      
+      setMovies(data.results);
+    };
+    fetchMovies();
+  }, []);
+  console.log(movies);
+
+return (
+  <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 
+  lg: grid-cols-5 xl:grid-cols-6'>
+    {
+      movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie}/>
+      ))}
+  </div>
+);
+}
