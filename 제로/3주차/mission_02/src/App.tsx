d7cb1774334085ff2e8ddef253dcbb64835bc853
제로 <dl0407132@gmail.com>
2026-04-05 00:53:48 +0900

    feat: 제로 3주차 미션2 제출

---
+++ 제로/3주차/mission_02/src/App.tsx
@@ -0,0 +1,54 @@
+import './App.css'
+import MoviePage from './pages/MoviePage'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import MovieDetailPage from './pages/MovieDetailPage'
+import { Navigate } from 'react-router-dom'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />,
+//     errorElement: <NotFoundPage />,
+//     children: [
+//       {
+//       path: 'movies/:category',
+//       element: <MoviePage />
+//       },
+//       {
+//         path: 'movies/:movieId',
+//         element: <MovieDetailPage />
+//       },
+//     ],
+//   },
+// ]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="movies/category/popular" />
+      },
+      {
+        path: 'movies/category/:category',   // ✅ 카테고리
+        element: <MoviePage />
+      },
+      {
+        path: 'movies/:movieId',             // ✅ 상세
+        element: <MovieDetailPage />
+      },
+    ],
+  },
+]);
+  
+
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App;
