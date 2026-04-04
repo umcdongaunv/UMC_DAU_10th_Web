e2911dd7fed1e358f3efb95146015e6ea60b7982
oreore0 <jhu5589870@gmail.com>
2026-04-04 09:53:02 +0900

    feat: 오레오 3주차 미션2 제출

--- OREO/week3/src/App.tsx
+++ OREO/week3/src/App.tsx
@@ -1,11 +1,29 @@
-import MoviePage from './01useEffect/page/MoviePage'; // pages → page
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './02Mission2/pages/HomePage';
+import MoviePage from './02Mission2/pages/MoviePage';
+import NotFoundPage from './02Mission2/pages/NotFoundPage';
+import { MovieDetailPage } from './02Mission2/pages/MovieDetailPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: 'movies/:category',
+        element: <MoviePage />
+      },
+      {
+        path: 'movie/:movieId',
+        element: <MovieDetailPage />
+      }
+    ],
+  },
+]);
 
 export const App = () => {
-  return (
-    <>
-      <MoviePage />
-    </>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
