85c6bf20fbedbc7a39e559e59a450cd133b58fa5
leehwayoon <dgdm1215@naver.com>
2026-04-11 14:46:24 +0900

    feat: 구름 4주차 미션3 제출

---
+++ hyun_seo/week_04/mission_03/src/layouts/HomeLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom"
+import { Navbar } from "../components/Navbar"
+
+export const HomeLayout = () => {
+  return (
+    <div className='h-dvh flex flex-col bg-black overflow-hidden'>
+      
+      <Navbar /> 
+      
+      <main className='flex-1 overflow-y-auto'>
+        <Outlet />
+      </main>
+      
+      <footer className='p-5 bg-black border-t border-gray-800 shrink-0'>
+        <p className='text-gray-500 text-sm font-light text-center'>
+          Currently learning React & TypeScript.
+        </p>
+      </footer>
+    </div>
+  )
+}
